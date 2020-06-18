330dcc8a01058900cf57ac58c058b204fa95ddd9
jaemin-jeong <jaemin-jeong@naver.com>
2020-06-18 15:06:40 +0900

    🚧 edit profile feature
    - 프로필 페이지 수정 기능 추가
    - 프로필 사진, 이름 변경 가능

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import routes from '../routes';
-import { userDetail, editProfile, changePassword } from '../controllers/userControllers';
-import { onlyPrivate } from '../middlewares';
+import {
+  userDetail, changePassword, getEditProfile, postEditProfile,
+} from '../controllers/userControllers';
+import { onlyPrivate, uploadAvatar } from '../middlewares';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
+
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
