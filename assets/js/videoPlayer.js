edae5b1ba89071a9fb2b72048c8d0f9be27eb16e
jaemin-jeong <jaemin-jeong@naver.com>
2020-06-23 21:12:19 +0900

    🚧 player control
    - 비디오 총 시간
    - 현재 진행 시간

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector('#jsVideoPlayer video');
 const playBtn = document.getElementById('jsPlayButton');
 const volumeBtn = document.getElementById('jsVolumeButton');
 const fullScreenBtn = document.getElementById('jsFullScreenButton');
+const currentTime = document.getElementById('currentTime');
+const totalTime = document.getElementById('totalTime');
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -25,24 +27,78 @@ function handleVolumeClick() {
 }
 
 function goFullScreen() {
-  videoContainer.requestFullscreen();
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen();
+    // firefox
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen();
+    // Chrome, Safari and Opera
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen();
+    // IE/Edge
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen();
+  }
   fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
   fullScreenBtn.removeEventListener('click', goFullScreen);
   fullScreenBtn.addEventListener('click', exitFullScreen);
 }
 
 function exitFullScreen() {
-  document.exitFullscreen();
-  fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
   fullScreenBtn.addEventListener('click', goFullScreen);
+  if (document.exitFullScreen) {
+    document.exitFullScreen();
+    // firefox
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+    // Chrome, Safari and Opera
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+    // IE/Edge
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
 }
 
+const formatDate = (seconds) => {
+  // 파라미터로 받은 초를 10진수로 변환해 저장
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function getCurrentTime() {
+  currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+function setTotalTime() {
+  // duration: 미디어의 길이를 초로 나타냄
+  console.log('videoPlayer ---> ', videoPlayer);
+  console.log('videoPlayer.duration ---> ', videoPlayer.duration);
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+}
+
+
 function init() {
-  // console.log(videoContainer);
-  // console.log(videoPlayer);
   playBtn.addEventListener('click', handlePlayClick);
   volumeBtn.addEventListener('click', handleVolumeClick);
   fullScreenBtn.addEventListener('click', goFullScreen);
+  videoPlayer.addEventListener('loadedmetadata', setTotalTime());
 }
 
 if (videoContainer) {
